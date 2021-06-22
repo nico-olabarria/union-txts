6800d043190d0529a0734a85c3506a5374bdf983
Nicolás <nolabarria.garcia@gmail.com>
2021-06-22 10:48:01 +0200

    Lectura de un archivo de texto
    Código para leer un solo archivo de texto

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+# Importación de librerías
 import pandas as pd
 
+# Lectura de las rutas de los archivos de texto
 with open("Rutas.txt","r") as route_file:
     rutas = route_file.readlines()
 
@@ -7,5 +9,10 @@ i = 0
 for ruta in rutas:
     rutas[i] = ruta.replace("\n", "")
     i += 1
-    
-print(rutas)
+
+# Lectura de los archivos de texto
+
+df_titles = pd.read_csv(rutas[0],sep = ';', skiprows = 29, nrows = 0, usecols = range(1, 14))
+df_rows = pd.read_csv(rutas[0],names = df_titles.columns,sep = ';', skiprows = 31, nrows = 140, usecols = range(1, 14))
+print(df_titles)
+print(df_rows)
