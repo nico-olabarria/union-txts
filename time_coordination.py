175b234a062e5d907968335846e20653516e67f4
Nicolás <nolabarria.garcia@gmail.com>
2021-06-23 10:28:52 +0200

    Lectura de primeras columnas como fechas

--- time_coordination.py
+++ time_coordination.py
@@ -3,11 +3,7 @@
 def time_coordination(df):
     import pandas as pd
     ref_route = input("Introduzca la ruta del excel que quiere utilizar como referencia: ")
-
-    ref_df = pd.read_csv(ref_route, sep = ',')
-
-    for i in range(0, len(ref_df), 2):
-        pd.to_datetime(ref_df.iloc[:, i])
-
+    variable = input("Introduzca el nombre de la variable tiempo: ")
+    ref_df = pd.read_csv(ref_route, sep = ',', parse_dates = [variable])
     print(ref_df.dtypes)
-
+    
