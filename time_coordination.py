02b7b186139ceb9acfb495f5bc16569245ae9a8c
Nicolás <nolabarria.garcia@gmail.com>
2021-06-23 10:14:41 +0200

    Creación bucle de conversión a fecha
    Conversión del dtype de las filas a datetime

--- time_coordination.py
+++ time_coordination.py
@@ -1,9 +1,13 @@
-import pandas as pd
+
 
 def time_coordination(df):
-
+    import pandas as pd
     ref_route = input("Introduzca la ruta del excel que quiere utilizar como referencia: ")
 
     ref_df = pd.read_csv(ref_route, sep = ',')
-    
+
+    for i in range(0, len(ref_df), 2):
+        pd.to_datetime(ref_df.iloc[:, i])
+
+    print(ref_df.dtypes)
 
